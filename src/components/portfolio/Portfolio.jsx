8d1eb34a5d21ad6bf0d4926ea14479cd6d8a2d51
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-06-22 16:00:56 +0200

    begin addind projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,21 @@
 import "./portfolio.scss";
 
+import Apotheose from "../../assets/screenshots/capture.png"
+
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            <h1>PORTFOLIO</h1>
-            <ul>
-                <li>
-
-                </li>
-            </ul>
+            <div className="left">
+                <div className="imgContainer">
+                    <img src={Apotheose} alt="apercu du formulaire d'inscription" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Projet de Fin d'Etudes</h2>
+                    <h1>C du Props</h1>
+                </div>
+            </div>
         </div>
     )
 }
