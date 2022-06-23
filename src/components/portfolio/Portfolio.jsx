72f3306074501818d5b493512f0fd4922bc9cd80
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-06-23 10:48:02 +0200

    project cduprops added ok

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import "./portfolio.scss";
 
 import Apotheose from "../../assets/screenshots/capture.png"
+import LogoReact from "../../assets/icons_skills/react.svg"
 
 export default function Portfolio() {
     return (
@@ -18,6 +19,7 @@ export default function Portfolio() {
                         <h3>C du Props</h3>
                         <p>C du Props est une application web permettant d'organiser les tâches ménagères en famille, en couple, ou entre amis de façon ludique.</p>
                         <p>L'application a été développée en 3 semaines par une équipe de 5 développeurs dont je faisais partie. </p>
+                        <p>Technos utilisées: REACT - Material UI</p>
                         <a href="https://youtu.be/-L8AGjsqhww?t=2796" target="_blank" rel="noreferrer">Voir la démo YouTube</a>
                         <a href="https://github.com/amelie-forgeard/projet-apo-front" target="_blank" rel="noreferrer">Voir le code source</a>
                     </div>
